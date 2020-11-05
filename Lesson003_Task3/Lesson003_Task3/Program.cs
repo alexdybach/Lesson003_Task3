46e98aa7c8b03fd50ba0990da851aeaeb9e911d3
Олександр Дибач <alex.dybach.2014@i.ua>
2020-11-05 16:33:38 +0200

    Done lesson003_Task3

---
+++ Lesson003_Task3/Lesson003_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson003_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Загадка: Хто завжди правду каже?\nВідповідь (english) :");
+            string answer = Console.ReadLine();
+
+            if (answer == "Mirror" || answer == "mirror" || answer == "MIRROR" || answer == "mIRROR")
+                Console.WriteLine("Відповідь правильна!");
+
+            else
+                Console.WriteLine("Відповідь неправильна!");
+        }
+    }
+}
